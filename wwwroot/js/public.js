bb4bb1206ba4bf853662721625a2d7af3a4620e2
魏水平 <wspnet@hotmail.com>
2019-11-27 19:52:23 +0800

    导航

--- wwwroot/js/public.js
+++ wwwroot/js/public.js
@@ -1,9 +1,16 @@
 $(window).load(function () {
   $(".navbar-toggler").click(function(){
 	$(this).toggleClass("navbar-toggler-click toggler_button");
-	$(".navbar ul").slideToggle(250);
+	$(".navbar>ul").slideToggle(250);
   });
-  $(".navbar ul li").each(function(index) {
+  $(".navbar>ul>li").each(function(index) {
 	$(this).css({'animation-delay':(index/10)+'s'});
   });
+});
+$(function () {
+    $(".navbar>ul>li").hover(function () {
+        $(this).children("ul").slideDown();
+    }, function () {
+        $(this).children("ul").stop(true, true).slideUp();
+    });
 });
